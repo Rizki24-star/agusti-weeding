8e877369eed5f1f7526ab64405f525d551c65a1b
Rizki24-star <kikingar.567@gmail.com>
2025-01-24 23:05:00 +0700

    test

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -8,13 +8,10 @@ import { default as bgRsv } from './bg_rsv.png';
 import { default as cardBow } from './card_bow.png';
 import { default as flowerTitle } from './flower_title.png';
 import { default as icInstragram } from './ic_instagram.png';
-import { default as imgOpening } from './img_opening.png';
 import { default as openingFlowerVertical } from './opening_flower_vertical.png';
 import { default as openingFlower } from './opening_flower.png';
 import { default as reply } from './ic_reply.png';
 import { default as sky } from './sky.gif';
-import { default as weeding1 } from './weeding_1.png';
-import { default as weeding2 } from './weeding_2.png';
 import { default as weedingSingle1 } from './weeding_single_1.png';
 import { default as weedingSingle2 } from './weeding_single_2.png';
 
@@ -34,8 +31,6 @@ export {
   openingFlowerVertical,
   reply,
   sky,
-  weeding1,
-  weeding2,
   weedingSingle1,
   weedingSingle2,
 };
